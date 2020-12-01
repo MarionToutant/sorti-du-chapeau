ad933d86ba92529d75155fa735969f7120436f55
Marion Toutant <63396949+MarionToutant@users.noreply.github.com>
2020-12-01 10:57:02 -0500

    tries post requests

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios')
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,9 +11,13 @@ router.get('/', function(req, res, next) {
 });
 
 /* POST send email */
-router.post('/sendEmail', function(req, res, next) {
+router.post('/sendEmail', async function(req, res, next) {
   console.log(req.body[0].email);
   
+  // using Twilio SendGrid's v3 Node.js Library
+  // https://github.com/sendgrid/sendgrid-nodejs
+  /*
+  javascript
   const sgMail = require('@sendgrid/mail')
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   const msg = {
@@ -23,13 +31,81 @@ router.post('/sendEmail', function(req, res, next) {
     .send(msg)
     .then(() => {
       console.log('Email sent')
+      
     })
     .catch((error) => {
-      console.error(error)
+      console.log(error)
+      
+    })
+    */
+
+    const response = await axios.post('https://api.sendgrid.com/v3/mail/send', {
+        "personalizations": [
+          {
+            "to": [
+              {
+                "email": "marion.toutant@gmail.com"
+              }
+            ],
+            "subject": "Hello, World! 2"
+          }
+        ],
+        "from": {
+          "email": "contact@tout-en-m.net"
+        },
+        "content": [
+          {
+            "type": "text/plain",
+            "value": "Hello, World!"
+          }
+        ]
+      }, {
+        headers: {
+          'Authorization': process.env.SENDGRID_API_KEY,
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+    .then(function (response) {
+      console.log(response.data);
+      console.log(response.status);
     })
 
+    /*
+    var response = request('POST', 'https://api.sendgrid.com/v3/mail/send', {
+      headers: {
+        'Content-Type' : 'application/json',
+        'Authorization' : `Bearer ${process.env.SENDGRID_API_KEY}`
+      },
+      json: {
+        "personalizations": [
+          {
+            "to": [
+              {
+                "email": "marion.toutant@gmail.com"
+              }
+            ],
+            "subject": "Hello, World! 2"
+          }
+        ],
+        "from": {
+          "email": "contact@tout-en-m.net"
+        },
+        "content": [
+          {
+            "type": "text/plain",
+            "value": "Hello, World!"
+          }
+        ]
+      }
+    })
 
-  res.json({ message: 'Réponse du backend' });
+    console.log(response.getBody())
+    */
+
+
+    res.json({ message: 'OK' });
+
 });
 
 module.exports = router;
